ec637405d2f3d0fcc89da9718eb47c770ecb21db
bigyoshio <albertogalvezmendoza00@gmail.com>
2026-04-13 22:29:58 -0700

    Correcciones y más ejemplos salu2

--- NormalizarAutores/norm_autores.py
+++ NormalizarAutores/norm_autores.py
@@ -4,26 +4,33 @@ import unicodedata
 def normalizar_autor(no_definido):
     if not no_definido or not isinstance(no_definido, str):
         return ""
+    # Quitar acentos
     nombre = unicodedata.normalize('NFKD', no_definido)
     nombre = "".join([c for c in nombre if not unicodedata.combining(c)])
     
     nombre = nombre.lower()
-
     nombre = re.sub(r'[^a-z\s,]', '', nombre)
 
+    # Lógica de la coma (Apellido, Nombre -> Nombre Apellido)
     if ',' in nombre:
-        partes = nombres.split(',')
-        nombre = f"{partes[1].strip()} {partes[0].strip()}"
+        partes = nombre.split(',')
+        if len(partes) >= 2:
+            nombre = f"{partes[1].strip()} {partes[0].strip()}"
+    
     nombre = " ".join(nombre.split())
     return nombre.title()
 
-#Ejemplo práctico 
-autores=[
+# ESTO ES LO QUE HACE QUE SALGA EN LA TERMINAL
+autores = [
     "Gálvez Mendoza Alberto",
-    "Papá"
+    "Papá",
+    "Gálvez Mendoza, Alberto",
+    "alberto galvez mendoza",
+    "Mendoza, A. G."
 ]
 
-print(f"{'Original':<30} | {'Normalizando':<30}")
+print("\n--- INICIANDO NORMALIZACIÓN ---")
+print(f"{'Original':<30} | {'Normalizado':<30}")
 print("-" * 65)
 
 for autor in autores:
